eb817542f6c482a1d48605a75c1d4a88d18b5482
Peng Li <pengli09@gmail.com>
2025-10-21 15:05:31 +0800

    add start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#########################################################################
+# File Name: start.sh
+# Author: Peng Li
+# Mail: pengli09@gmail.com
+# Created Time: 2025-10-21 15:04:14
+#########################################################################
+hugo server -D --ignoreCache --disableFastRender
